2af2bf0f6a3020503068d2ba3d510b4188e363ee
Chasy <chaseyeon02@gmail.com>
2025-11-12 00:59:30 +0900

    메인 씬, 악타 메인 페이지, 뉴스 완성
    [완성]
    - #01메인 씬
    - 악타 씬 중에서 메인, 뉴스 페이지 완성
    
    [진행할 것]
    - 악타 씬 나머지 플랫폼 페이지
    - #02게임 인트로 씬
    - #03뉴스 인트로 씬
    - 컴퓨터 메인 화면, 퀘스트 화면 등
    
    [수정할 것]
    - 악타에서 뉴스 페이지 스크롤을 내리고 다시 악타 메인 페이지로 돌아왔을 때 스크롤 위치가 같음(active될때마다 위치 초기화되도록 수정하기)

---
+++ D_31/Assets/Scripts/#01Main/MainManager.cs
@@ -0,0 +1,39 @@
+using System.Collections; // 코루틴을 사용하기 위해 추가
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject StartButton;
+
+    [SerializeField]
+    private GameObject ExitButton;
+
+    [SerializeField]
+    private GameObject CreditPanel; 
+
+    void Start()
+    {
+        if (CreditPanel != null) CreditPanel.SetActive(false);
+
+    }
+    
+    // // 게임 시작 함수
+    // public void StartGame()
+    // {
+    //     // "시작" 씬을 로드
+    //     SceneManager.LoadScene("");
+    // }
+
+    // 게임 종료 함수
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
